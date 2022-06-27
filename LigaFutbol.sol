4de442290a0636fa2fc27b4fc9df936ff55443d7
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-06-27 17:14:43 -0300

    Create LigaFutbol.sol

---
+++ LigaFutbol.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract TorneoFutbol {
+    mapping(string => string) _jugadores;
+    mapping(string => uint256) _puntos;
+    string[] private _jugadores;
+    uint256[] private _puntos;
+}
+
+

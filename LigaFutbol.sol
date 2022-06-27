bc6c461b6063bdf99c2c3e646c7410247ff1dea4
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-06-27 17:19:06 -0300

    casi termiando

--- LigaFutbol.sol
+++ LigaFutbol.sol
@@ -2,10 +2,29 @@
 pragma solidity ^0.8.14;
 
 contract TorneoFutbol {
-    mapping(string => string) _jugadores;
-    mapping(string => uint256) _puntos;
+    mapping(string => string) jugadores__equipo;
+    mapping(string => uint256) puntos__equipo;
     string[] private _jugadores;
-    uint256[] private _puntos;
+        uint256[] private _puntos;
 }
 
+function addEquipo(string memory _equipo) public {
+    jugadores__equipo[_equipo] = "";
+    puntos__equipo[_equipo] = 0;
+}
 
+function addJugador (string memory _equipo, string memory _jugador) public {
+    jugadores__equipo[_equipo] = ;
+}
+
+function addPuntos(string memory _equipo, uint256 _puntos) public {
+    puntos__equipo[_equipo] = ;
+}
+
+function getPuntos(string memory _equipo) public view returns (uint256) {
+    return puntos__equipo[_equipo];
+}
+
+function getJugadores(string memory _equipo) public view returns (string[]) {
+    return jugadores__equipo[_equipo];
+}

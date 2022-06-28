87ed839b0e2de93079e12c669adc064cb82ae1fb
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-06-28 18:31:17 -0300

    Update LigaFutbol.sol

--- LigaFutbol.sol
+++ LigaFutbol.sol
@@ -13,17 +13,43 @@ function addEquipo(string memory _equipo) public {
     puntos__equipo[_equipo] = 0;
 }
 
+function deleteEquipo(string memory _equipo) public {
+    delete jugadores__equipo[_equipo];
+    delete puntos__equipo[_equipo];
+}
+
+function deleteJugador(string memory _jugador) public {
+    delete jugadores__equipo[_jugador];
+}
+
 function addJugador (string memory _equipo, string memory _jugador) public {
     jugadores__equipo[_equipo] = _jugador; 
 }
 
-function addPuntos(string memory _equipo, uint256 _puntos) public {
-    puntos__equipo[_equipo] = ;
+function addPuntos (
+    string memory _1equipo,
+    string memory _2equipo,
+) public {
+    if (jugadores__equipo[_1equipo] == jugadores__equipo[_2equipo]) {
+        puntos__equipo[_1equipo] += 1;
+        puntos__equipo[_2equipo] += 1;
+    } else if {
+        puntos__equipo[_1equipo] += 3;
+        puntos__equipo[_2equipo] += 0;
+    }
+    else {
+        puntos__equipo[_1equipo] += 0;
+        puntos__equipo[_2equipo] += 3;
+    }
 }
 
-function getPuntos(string memory _equipo) public view returns (uint256) {
-    return puntos__equipo[_equipo];
-}
+
+function get_puntos_equipo(string memory equipo_)
+        public view
+        returns (uint256)
+    {
+        return puntos_equipo[equipo_];
+    }
 
 function getJugadores(string memory _equipo) public view returns (string[]) {
     return jugadores__equipo[_equipo];

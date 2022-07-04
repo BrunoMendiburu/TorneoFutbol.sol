4579bcabe477e52aeaaa32ef1f27ba8491f81706
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-07-04 10:13:33 -0300

    Update LigaaFutbol.sol

--- LigaaFutbol.sol
+++ LigaaFutbol.sol
@@ -1,77 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-contract LigaFutbol {
-    mapping(string => string) jugadores__equipo;
-    mapping(string => uint256) puntos__equipo;
-    string[] private _jugadores;
-    uint256[] private _puntos;
+contract Tournament {
+    mapping(string => string[]) private jugadores__equipo;
+    mapping(string => uint256[]) private puntos__equipo;
 
-    function addJugadoresEquipo(string memory _equipo, string memory _jugador)
+    function setAddJugadoresEquipo(string memory jugador_, string memory equipo_)
         public
     {
-        jugadores__equipo[_equipo] = _jugador;
+        jugadores__equipo[equipo_].push(jugador_);
     }
 
-    function deleteJugadoresEquipo(string memory _jugador) public view {
-        for (uint256 i = 0; i < _jugadores.length; i++) {
+    function delJugadoresEquipo(string memory jugador_) public {
+        for (uint256 i = 0; i < jugadores__equipo[jugador_].length; i++) {
             if (
-                keccak256(abi.encodePacked(_jugadores[i])) ==
-                keccak256(abi.encodePacked(_jugador))
+                keccak256(abi.encodePacked(jugadores__equipo[jugador_][i])) ==
+                keccak256(abi.encodePacked(jugador_))
             ) {
-                delete _jugador;
+                delete jugadores__equipo[jugador_][i];
             }
         }
     }
 
     function addPuntosEquipo(
-        string memory _1equipo,
-        string memory _2equipo,
-        uint256 goals_1,
-        uint256 goals_2
+        string memory equipo1_,
+        string memory equipo2_,
+        uint256 _1goles,
+        uint256 _2goles
     ) public {
-        if (goals_1 == goals_2) {
-            puntos__equipo[_1equipo] += 1;
-            puntos__equipo[_2equipo] += 1;
-        } else if (goals_1 > goals_2) {
-            puntos__equipo[_1equipo] += 3;
-            puntos__equipo[_2equipo] += 0;
+        if (_1goles > _2goles) {
+            puntos__equipo[equipo1_].push(3);
+            puntos__equipo[equipo2_].push(0);
+        } else if (_1goles < _2goles) {
+            puntos__equipo[equipo1_].push(0);
+            puntos__equipo[equipo2_].push(3);
         } else {
-            puntos__equipo[_1equipo] += 0;
-            puntos__equipo[_2equipo] += 3;
+            puntos__equipo[equipo1_].push(1);
+            puntos__equipo[equipo2_].push(1);
         }
     }
 
-    function getPuntosEquipo(string memory equipo__)
-        public
-        view
+    function addPuntosEquipo(string memory _equipo)
+        public view
         returns (uint256)
     {
-        uint256 puntos = 0;
-        for (uint256 i = 0; i < _puntos.length; i++) {
-            puntos += _puntos[i]; // decirle a chona
+        uint256 addPuntos = 0;
+        for (uint256 i = 0; i < puntos__equipo[_equipo].length; i++) {
+            addPuntos += puntos__equipo[_equipo][i];
         }
-        return puntos;
+        return addPuntos;
     }
 
     function getJugadoresEquipo() public view returns (string[] memory) {
-        string[] memory jugadores = new string[](_jugadores.length);
-        for (uint256 i = 0; i < jugadores.length; i++) {
-            jugadores[i] = jugadores[i];
+        string memory _equipo;
+        string[] memory jugadores = new string[](
+            jugadores__equipo[_equipo].length
+        );
+        for (uint256 i = 0; i < jugadores__equipo[_equipo].length; i++) {
+            jugadores[i] = jugadores__equipo[_equipo][i];
         }
         return jugadores;
     }
 
-    function getEquipoMasPuntos() public view returns (string memory) {
-        uint256 puntos_max = 0;
-        string memory equipo;
-        string memory equipo_mas_puntos = "";
-        for (uint256 i = 0; i < _puntos.length; i++) {
-            if (getPuntosEquipo(equipo) > puntos_max) {
-                puntos_max = getPuntosEquipo(equipo);
-                equipo_mas_puntos = equipo;
+    function equipoMasPuntos() public view returns (string memory) {
+        uint256 mas_puntos = 0;
+        string memory equipo_mas = "";
+        string memory equipo_;
+        for (uint256 i = 0; i < puntos__equipo[equipo_].length; i++) {
+            if (addPuntosEquipo(equipo_) > mas_puntos) {
+                mas_puntos = addPuntosEquipo(equipo_);
+                equipo_mas = equipo_;
             }
         }
-        return equipo_mas_puntos;
+        return equipo_mas;
     }
 }
